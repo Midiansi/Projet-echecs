a0da22b5d904564f8783e4f50a97cf08bfabe45d
Joé <99928569+ManoPixel@users.noreply.github.com>
2022-08-25 20:11:52 -0300

    Update 1.2.py

--- 1.2.py
+++ 1.2.py
@@ -51,14 +51,14 @@ def echiquier_vide():
         for j in range(8):
             if i % 2 == 0:
                 if j % 2 == 0:
-                    pyxel.rect(i * 16, j * 16, 16, 16, couleur_1)
+                    pyxel.rect(j * 16, i * 16, 16, 16, couleur_1)
                 else:
-                    pyxel.rect(i * 16, j * 16, 16, 16, couleur_2)
+                    pyxel.rect(j * 16, i * 16, 16, 16, couleur_2)
             else:
                 if j % 2 == 0:
-                    pyxel.rect(i * 16, j * 16, 16, 16, couleur_2)
+                    pyxel.rect(j * 16, i * 16, 16, 16, couleur_2)
                 else:
-                    pyxel.rect(i * 16, j * 16, 16, 16, couleur_1)
+                    pyxel.rect(j * 16, i * 16, 16, 16, couleur_1)
                     
 def dessiner_piece(tab):
     """Dessine une piece qui est dans tab
@@ -66,21 +66,21 @@ def dessiner_piece(tab):
     
     for i in range(8):
         for j in range(8):
-            case = tab[j][i]
+            case = tab[i][j]
             if case != 0: #Case non vide
                 
                 if case == 1: #Dessine un pion
-                    pyxel.blt(i * 16, j * 16, 0, 0, 0, 16, 16, 11)
+                    pyxel.blt(j * 16, i * 16, 0, 0, 0, 16, 16, 11)
                 elif case == 2: #Dessine un cheval
-                    pyxel.blt(i*16, j*16, 0, 16, 0, 16, 16, 11)
+                    pyxel.blt(j * 16, i * 16, 0, 16, 0, 16, 16, 11)
                 elif case == 3: #Dessine une tour
-                    pyxel.blt(i*16, j*16, 0, 32, 0, 16, 16, 11)
+                    pyxel.blt(j * 16, i * 16, 0, 32, 0, 16, 16, 11)
                 elif case == 4: #Dessine une reine
-                    pyxel.blt(i*16, j*16, 0, 48, 0, 16, 16, 11)
+                    pyxel.blt(j * 16, i * 16, 0, 48, 0, 16, 16, 11)
                 elif case == 5: #Dessine un fou
-                    pyxel.blt(i*16, j*16, 0, 0, 16, 16, 16, 11)
+                    pyxel.blt(j * 16, i * 16, 0, 0, 16, 16, 16, 11)
                 elif case == 6: #Dessine un roi
-                    pyxel.blt(i*16, j*16, 0, 16, 16, 16, 16, 11)
+                    pyxel.blt(j * 16, i * 16, 0, 16, 16, 16, 16, 11)
                     
                     
 def update():
