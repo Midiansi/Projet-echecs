df60f2c7be48d5f968eab4c7dc240d41d02df023
Joé <99928569+ManoPixel@users.noreply.github.com>
2022-08-26 21:39:58 -0300

    Add files via upload

---
+++ 1.5.py
@@ -0,0 +1,138 @@
+# ECHEC
+
+import pyxel
+
+pyxel.init(128, 128, title="Échec")
+pyxel.mouse(True)
+
+pyxel.load("my_resource.pyxres", True, True, True, True)
+    
+echiquier = [[0] * 8 for loop in range (8)]
+jeu = "echecs"
+
+case_gauche = "" #Case selectionné clic gauche
+case_droite = "" #Case selectionné clic droit
+
+def remplir_chiquier_initiallement(tab, jeu):
+    """Prends un tableau vide et met les pieces conforme le jeu choisi"""
+    
+    if jeu == "echecs":
+        for i in range(8):
+            tab[1][i] = "1b" #ligne de pions bleu
+            tab[6][i] = "1r" #ligne de pions roses
+            
+        tab[0][1], tab[0][6] = "2b", "2b" #chevaux bleu
+        tab[7][1], tab[7][6] = "2r", "2r" #chevaux rose
+        
+        tab[0][0], tab[0][7] = "3b", "3b" #tours bleu
+        tab[7][0], tab[7][7] = "3r", "3r" #tours rose
+        
+        tab[0][3] = "4b" #reine bleu
+        tab[7][3] = "4r" #reine rose
+        
+        tab[0][2], tab[0][5] = "5b", "5b" #fou bleu
+        tab[7][2], tab[7][5] = "5r", "5r" #fou rose
+        
+        tab[0][4] = "6b" #roi bleu
+        tab[7][4] = "6r" #roi rose
+            
+#    if jeu == "dames":
+            
+    return tab
+        
+    
+def echiquier_vide():
+    """Dessine un échiquier vide au fond"""
+    
+    couleur_1 = 7
+    couleur_2 = 4
+    
+    for i in range(8):
+        for j in range(8):
+            if i % 2 == 0:
+                if j % 2 == 0:
+                    pyxel.rect(j * 16, i * 16, 16, 16, couleur_1)
+                else:
+                    pyxel.rect(j * 16, i * 16, 16, 16, couleur_2)
+            else:
+                if j % 2 == 0:
+                    pyxel.rect(j * 16, i * 16, 16, 16, couleur_2)
+                else:
+                    pyxel.rect(j * 16, i * 16, 16, 16, couleur_1)
+                    
+def dessiner_piece(tab):
+    """Dessine une piece qui est dans tab
+    0 est supposé une case vide"""
+    
+    for i in range(8):
+        for j in range(8):
+            
+            case = tab[i][j]
+            
+            if case != 0: #Case non vide
+                
+                piece = int(case[0]) #Piece = 1 ou 2 .. ou 6
+                couleur = case[1] #Couleur = b ou r
+                
+                if couleur == "b":
+                    ligne_couleur = 0
+                    
+                elif couleur == "r":
+                    ligne_couleur = 16
+                
+                if piece == 1: #Dessine un pion
+                    pyxel.blt(j * 16, i * 16, 0, 0, ligne_couleur, 16, 16, 11)
+                elif piece == 2: #Dessine un cheval
+                    pyxel.blt(j * 16, i * 16, 0, 16, ligne_couleur, 16, 16, 11)
+                elif piece == 3: #Dessine une tour
+                    pyxel.blt(j * 16, i * 16, 0, 32, ligne_couleur, 16, 16, 11)
+                elif piece == 4: #Dessine une reine
+                    pyxel.blt(j * 16, i * 16, 0, 48, ligne_couleur, 16, 16, 11)
+                elif piece == 5: #Dessine un fou
+                    pyxel.blt(j * 16, i * 16, 0, 64, ligne_couleur, 16, 16, 11)
+                elif piece == 6: #Dessine un roi
+                    pyxel.blt(j * 16, i * 16, 0, 80, ligne_couleur, 16, 16, 11)
+                    
+def clic_case():
+    """Le clic de la souris correspond à une position dans le tableau"""
+    return pyxel.mouse_y  // 16, pyxel.mouse_x // 16
+
+def echange_tab(tab, case_gauche, case_droite):
+    """Echange pièces"""
+    g_i, g_j = case_gauche[0], case_gauche[1]
+    d_i, d_j = case_droite[0], case_droite[1]
+    
+    
+    tmp = tab[g_i][g_j]
+    tab[g_i][g_j] = tab[d_i][d_j]
+    tab[d_i][d_j] = tmp
+    
+    case_gauche, case_droite = "", ""    
+    return tab, case_gauche, case_droite
+    
+#Fonction à mettre dans le menu de selection de jeu
+remplir_chiquier_initiallement(echiquier, jeu)
+
+def update():
+    global case_gauche, case_droite
+    
+    if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
+        case_gauche = clic_case()
+        
+    if pyxel.btnp(pyxel.MOUSE_BUTTON_RIGHT):
+        case_droite = clic_case()
+    
+    if case_gauche != "" and case_droite != "":
+        tab, case_gauche, case_droite = echange_tab(echiquier, case_gauche, case_droite)
+    
+def draw():     
+    echiquier_vide()
+
+    if case_gauche != "":
+        pyxel.rect(case_gauche[1] * 16, case_gauche[0] * 16, 16, 16, 5)
+    if case_droite != "":
+        pyxel.rect(case_droite[1] * 16, case_droite[0] * 16, 16, 16, 8)
+    
+    dessiner_piece(echiquier)
+            
+pyxel.run(update, draw)
